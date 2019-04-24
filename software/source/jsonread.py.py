f8429e4b30fd91cc9fa355bb09eec27833b24adc
Unknown <unknown@example.com>
2019-04-24 18:39:58 +0530

    Reading JSON file and writing to macid wise file.

---
+++ software/source/jsonread.py.py
@@ -0,0 +1,17 @@
+import json
+
+def readfile():
+	myfile=open("Non-Connected_Data_G2.txt","r")
+	while(1):
+		content=myfile.readline()
+	#print(content)
+		if content:
+			file_obj = json.loads(content)
+			file_mac=file_obj["SensorUUID"]
+			print(file_obj)
+			print(file_mac)
+	
+
+	
+readfile()
+	
